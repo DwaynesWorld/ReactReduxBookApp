ed670c528e7d1a340af7868c786f77998ff5307d
DwaynesWorld <kthompson713@gmail.com>
2018-07-16 00:31:14 -0500

    Added  book details

--- src/reducers/reducer_books.jsx
+++ src/reducers/reducer_books.jsx
@@ -1,9 +1,24 @@
 export default function() {
   return [
-    { title: "The Lord of The Rings" },
-    { title: "The Hitchhiker's Guide to the Galaxy" },
-    { title: "Ignition: An Informal History of Liquid Rocket Propellant" },
-    { title: "Structures: Or Why Things Don't Fall Down" },
-    { title: "Superintelligence: Paths, Dangers, Strategies" }
+    {
+      title: "The Lord of The Rings",
+      pages: 332
+    },
+    {
+      title: "The Hitchhiker's Guide to the Galaxy",
+      pages: 177
+    },
+    {
+      title: "Ignition: An Informal History of Liquid Rocket Propellant",
+      pages: 99
+    },
+    {
+      title: "Structures: Or Why Things Don't Fall Down",
+      pages: 188
+    },
+    {
+      title: "Superintelligence: Paths, Dangers, Strategies",
+      pages: 212
+    }
   ];
 }
